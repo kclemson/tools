ea6397e5df312075e706b1a2e61b81dd77a36829
kclemson <git@photo.lemson.com>
2021-03-24 18:44:16 -0700

    more descriptive help text

---
+++ bash/git-history-of-subdirs.bash
@@ -0,0 +1,73 @@
+#!/usr/bin/bash
+
+# Simple script to export the git logs for a set of subdirectories within a git repo 
+
+usage()  
+{  
+cat <<-EOF
+
+Usage: git-history-of-subdirs [-p path-to-directory] [-d number-of-days] [-e to export details to a file]
+
+Description: 
+    Exports git logs of all direct subfolders of the given path.
+
+Options:
+    -p path to the parent folder - required.
+    -d number of days' worth of history to export. If not specified, defaults to 90.
+    -e exports a CSV of the results. If not specified, subdir & commit count is just summarized on stdout.
+
+EOF
+    exit -1
+} 
+
+while getopts ":p:d:e" o; do
+    case "${o}" in
+        p)
+            path_to_scan=${OPTARG}
+            ;;
+        d)
+            number_of_days=${OPTARG}
+            ;;
+        e)
+            export_to_file=true    
+            starting_dir=$PWD
+            echo -n "" > "$starting_dir/export.txt"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+#path is a required input
+if [ -z "${path_to_scan}" ]; then
+    usage
+else
+    cd "$path_to_scan" || exit
+fi
+
+#if -d wasn't specified, default to 90
+if [ "$number_of_days" == "" ]; then
+    number_of_days="90 days ago"
+else
+    number_of_days=$number_of_days" days ago"
+fi
+
+for current_dir in $(find . -maxdepth 1 -type d); do (
+    # skip the current directory, only want stats for subdirectories
+    if [ "$current_dir" != "." ]
+    then
+        # Always spit out the basic stats to stdout
+        number_of_git_changes_in_subdir=$(git log --no-merges --after="${number_of_days} days ago" --pretty="${path_to_scan},${current_dir},%H,%cs,%cl,%cs,%s" -- "$current_dir" | wc -l)
+        (echo "git log entries for $current_dir: $number_of_git_changes_in_subdir") | tr -d "\n" 
+        echo "" 
+
+        # only export the results to a file if -e is specified
+        if [ "$export_to_file" ]
+        then
+            (git log --no-merges --after="${number_of_days} days ago" --pretty="${path_to_scan},${current_dir},%H,%cs,%cl,%cs,%s" -- "$current_dir") >> "$starting_dir"/export.txt
+        fi
+    fi
+); done
+
